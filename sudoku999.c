8515d86ac7c0e71863fd395ffc5b81d90482859a
VenuysTech <71267289+VenuysTech@users.noreply.github.com>
2020-10-20 22:26:45 -0300

    Update sudoku999.c

--- sudoku999.c
+++ sudoku999.c
@@ -1,72 +1,3 @@
-/***************************************************************************
- *   sudoku999.c                           Version 20201018.020303         *
- *                                                                         *
- *   Sudoku Solver                                                         *
- *   Copyright (C) 2016-2021    by Ruben Carlo Benante                     *
- *                                                                         *
- *                                 Leandro Dantas Lima                     *
- *                                                                         *
- ***************************************************************************
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; version 2 of the License.               *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************
- *   To contact the author, please write to:                               *
- *                                                                         *
- *   Ruben Carlo Benante                                                   *
- *   Email: rcb@beco.cc                                                    *
- *   Webpage: http://www.beco.cc                                           *
- *   Phone: +55 (81) 3184-7555                                             *
- *                                                                         *
- *   Leandro Dantas Lima                                                   *
- *   Email: leandroautocontrole@gmail.com                                  *
- *   Webpage: http://beco.poli.br/ldls3                                    *
- *   Phone: +55 (81) 98861-9469                                            *
- *                                                                         *
- ***************************************************************************/
-
-/* ---------------------------------------------------------------------- */
-/**
- * @file sudoku999.c
- * @ingroup GroupUnique
- * @brief Brief description
- * @details This program really do a nice job as a template, and template only!
- * @version 20160908.182830
- * @date 2016-09-08
- * @author Ruben Carlo Benante <<rcb@beco.cc>>
- * @par Webpage
- * <<a href="http://www.beco.cc">www.beco.cc</a>>
- * @copyright (c) 2016 GNU GPL v2
- * @note This program is free software: you can redistribute it
- * and/or modify it under the terms of the
- * GNU General Public License as published by
- * the Free Software Foundation version 2 of the License.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program.
- * If not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place - Suite 330, Boston, MA. 02111-1307, USA.
- * Or read it online at <<http://www.gnu.org/licenses/>>.
- *
- *
- * @todo Now that you have the template, hands on! Programme!
- * @warning Be carefull not to lose your mind in small things.
- * @bug This file right now does nothing usefull
- *
- */
 
 /*
  * Instrucoes para compilar:
@@ -93,56 +24,7 @@
 #include <stdlib.h> /* Miscellaneous functions (rand, malloc, srand)*/
 #include "sudoku999.h" /* To be created for this template if needed */
 
-/* #include <time.h> */ /* Time and date functions */
-/* #include <math.h> */ /* Mathematics functions */
-/* #include <string.h> */ /* Strings functions definitions */
-/* #include <dlfcn.h> */ /* Dynamic library */
-/* #include <malloc.h> */ /* Dynamic memory allocation */
-/* #include <unistd.h> */ /* UNIX standard function */
-/* #include <limits.h> */ /* Various C limits */
-/* #include <ctype.h> */ /* Character functions */
-/* #include <errno.h> */ /* Error number codes errno */
-/* #include <signal.h> */ /* Signal processing */
-/* #include <pthread.h> */ /* Parallel programming with threads */
-/* #include <ncurses.h> */ /* Screen handling and optimisation functions */
-/* #include <allegro.h> */ /* A game library for graphics, sounds, etc. */
-/* #include <libintl.h> */ /* Internationalization / translation */
-/* #include <locale.h> */ /* MACROS LC_ for location specific settings */
 
-/* ---------------------------------------------------------------------- */
-/**
- * @ingroup GroupUnique
- * @brief This is the main event of the evening
- * @details Ladies and Gentleman... It's tiiiime!
- * Fightiiiiing at the blue corner,
- * he, who has compiled more C code than any other adversary in the history,
- * he, who has developed UNIX and Linux, and is an inspiration to maaany languages
- * and compilers, the GNU C Compiler, GCC!
- * Fightiiiiing at the red corner, the challenger, in his first fight, lacking of any
- * valid experience but angrily, blindly, and no doubtfully, will try to
- * compile this program without errors. He, the student, the apprentice,
- * the developer, beco!!
- *
- * @param[in] argc Argument counter
- * @param[in] argv Argument strings (argument values)
- *
- * @retval 0 If succeed (EXIT_SUCCESS).
- * @retval 1 Or another error code if failed.
- *
- * @par Example
- * @code
- *    $./sudoku999 -h
- * @endcode
- *
- * @warning   Be carefull with...
- * @bug There is a bug with...
- * @todo Need to do...
- * @note You can read more about it at <<a href="http://www.beco.cc">www.beco.cc</a>>
- * @author Ruben Carlo Benante
- * @version 20160908.182830
- * @date 2016-09-08
- *
- */
 int main(void)
 {
     int z; /* flag indicativa de deducao com sucesso */
@@ -172,8 +54,6 @@ int main(void)
     return check(sudoku);
 }
 
-/* Write your functions here... */
-
 /* ---------------------------------------------------------------------- */
 /* funcoes de apoio */
 
@@ -185,52 +65,6 @@ void printsudoku(int w[9][9][9])
     printf("       a   b   c   d   e   f   g   h   i \n");
 }
 
-/* ---------------------------------------------------------------------- */
-/**
- * @ingroup GroupUnique
- * @brief Prints help information and exit
- * @details Prints help information (usually called by opt -h)
- * @return Void
- * @author Ruben Carlo Benante
- * @version 20160908.182830
- * @date 2016-09-08
- *
- */
-void help(void)
-{
-    IFDEBUG("help()");
-    printf("%s - %s\n", "sudoku999", "Brief description");
-    printf("\nUsage: %s [-h|-v]\n", "sudoku999");
-    printf("\nOptions:\n");
-    printf("\t-h,  --help\n\t\tShow this help.\n");
-    printf("\t-V,  --version\n\t\tShow version and copyright information.\n");
-    printf("\t-v,  --verbose\n\t\tSet verbose level (cumulative).\n");
-    /* add more options here */
-    printf("\nExit status:\n\t0 if ok.\n\t1 some error occurred.\n");
-    printf("\nTodo:\n\tLong options not implemented yet.\n");
-    printf("\nAuthor:\n\tWritten by %s <%s>\n\n", "Ruben Carlo Benante", "rcb@beco.cc");
-    exit(EXIT_FAILURE);
-}
-
-/* ---------------------------------------------------------------------- */
-/**
- * @ingroup GroupUnique
- * @brief Prints version and copyright information and exit
- * @details Prints version and copyright information (usually called by opt -V)
- * @return Void
- * @author Ruben Carlo Benante
- * @version 20160908.182830
- * @date 2016-09-08
- *
- */
-void copyr(void)
-{
-    IFDEBUG("copyr()");
-    printf("%s - Version %s\n", "sudoku999", VERSION);
-    printf("\nCopyright (C) %d %s <%s>, GNU GPL version 2 <http://gnu.org/licenses/gpl.html>. This  is  free  software: you are free to change and redistribute it. There is NO WARRANTY, to the extent permitted by law. USE IT AS IT IS. The author takes no responsability to any damage this software may inflige in your data.\n\n", 2016, "Ruben Carlo Benante", "rcb@beco.cc");
-    if(verb > 3) printf("copyr(): Verbose: %d\n", verb); /* -vvvv */
-    exit(EXIT_FAILURE);
-}
 
 /* ---------------------------------------------------------------------- */
 /**
@@ -238,40 +72,7 @@ void copyr(void)
  * @brief This function initializes some operations before start
  * @details Details to be written in
  * multiple lines
- *
- * @pre You need to call foo() before calling this function
- *
- * @param[in] i Input parameter that does bla
- * @param[out] o Parameter that outputs ble
- * @param[in,out] z The @a z variable is used as input and output
- *
- * @retval 0 Returned when bli
- * @retval 1 Error code: function returned blo
- *
- * @par Example
- * @code
- *    if(x == funcexample(i, o, z))
- *       printf("And that is it\n");
- * @endcode
- *
- * @return Void
- *
- * @warning Be carefull with blu
- * @todo Need to implement it. Its empty now. This doxygen tags are overwhelming.
- * Mandatory tags are: ingroup, brief, details, param, return, author and date.
- * The others are optional.
- *
- * @deprecated This function will be deactivated in version +11
- * @see help()
- * @see copyr()
- * @bug There is a bug with x greater than y
- * @note You can read more about it at <<a href="http://www.beco.cc">www.beco.cc</a>>
- *
- * @author Ruben Carlo Benante
- * @version 20160908.182830
- * @date 2016-09-08
- * @copyright Use this tag only if not the same as the whole file
- *
+
  */
 void sudoku999_init(int w[9][9][9])
 {
@@ -747,7 +548,3 @@ int tenta8(int w[9][9][9])
     char *hd="tenta8: backtracking";
     return z>0;
 }
-
-/* ---------------------------------------------------------------------- */
-/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
-/* Template by Dr. Beco <rcb at beco dot cc> Version 20160612.142044      */
