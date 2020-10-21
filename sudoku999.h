a29d4f63a3fd26515fe290c9ac218840b78f2bf1
VenuysTech <71267289+VenuysTech@users.noreply.github.com>
2020-10-20 21:40:28 -0300

    Update sudoku999.h

--- sudoku999.h
+++ sudoku999.h
@@ -1,109 +1,8 @@
-/***************************************************************************
- *   sudoku999.h                           Version 20201018.020303         *
- *                                                                         *
- *                                                                         *
- *   Sudoku Solver - header file                                           *
- *   Copyright (C) 2016-2021    by Ruben Carlo Benante                     *
- *                                                                         *
- *                                 Leandro Dantas Lima                     *
- *                                                                         *
- ***************************************************************************
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; version 2 of the License.               *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************
- *   To contact the author, please write to:                               *
- *                                                                         *
- *   Ruben Carlo Benante                                                   *
- *   Email: rcb@beco.cc                                                    *
- *   Webpage: http://www.beco.cc                                           *
- *   Phone: +55 (81) 3184-7555                                             *
- *                                                                         *
- *   Leandro Dantas Lima                                                   *
- *   Email: leandroautocontrole@gmail.com                                  *
- *   Webpage: http://beco.poli.br/ldls3                                    *
- *   Phone: +55 (81) 98861-9469                                            *
- *                                                                         *
- ***************************************************************************/
-
-/* ---------------------------------------------------------------------- */
-/**
- * \file sudoku999.h
- * \ingroup GroupUnique
- * \brief Brief description
- * \details This program really do a nice job as a template, and template only!
- * \version 20160908.182854
- * \date 2016-09-08
- * \author Ruben Carlo Benante <<rcb@beco.cc>>
- * \par Webpage
- * <<a href="www.beco.cc">www.beco.cc</a>>
- * \copyright (c) 2016 GNU GPL v2
- * \note This program is free software: you can redistribute it
- * and/or modify it under the terms of the
- * GNU General Public License as published by
- * the Free Software Foundation version 2 of the License.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program.
- * If not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place - Suite 330, Boston, MA. 02111-1307, USA.
- * Or read it online at <<http://www.gnu.org/licenses/>>.
- *
- *
- * \todo Now that you have the template, hands on! Programme!
- * \warning Be carefull not to lose your mind in small things.
- * \bug This file right now does nothing usefull
- *
- */
-
 #ifndef _EXN_H
 #define _EXN_H
 
 /* ---------------------------------------------------------------------- */
 /* includes */
-
-/* #include <stdio.h> */ /* Standard I/O functions */
-/* #include <stdlib.h> */ /* Miscellaneous functions (rand, malloc, srand)*/
-/* #include <getopt.h> */ /* get options from system argc/argv */
-
-/* #include <time.h> */ /* Time and date functions */
-/* #include <math.h> */ /* Mathematics functions */
-/* #include <string.h> */ /* Strings functions definitions */
-/* #include <dlfcn.h> */ /* Dynamic library */
-/* #include <malloc.h> */ /* Dynamic memory allocation */
-/* #include <unistd.h> */ /* UNIX standard function */
-/* #include <limits.h> */ /* Various C limits */
-/* #include <ctype.h> */ /* Character functions */
-/* #include <errno.h> */ /* Error number codes errno */
-/* #include <signal.h> */ /* Signal processing */
-/* #include <stdarg.h> */ /* Functions with variable arguments */
-/* #include <pthread.h> */ /* Parallel programming with threads */
-/* #include <fcntl.h> */ /* File control definitions */
-/* #include <termios.h> */ /* Terminal I/O definitions POSIX */
-/* #include <sys/stat.h> */ /* File status and information */
-/* #include <float.h> */ /* Float constants to help portability */
-/* #include <setjmp.h> */ /* Bypass standard function calls and return */
-/* #include <stddef.h> */ /* Various types and MACROS */
-/* #include <SWI-Prolog.h> */ /* Prolog integration with C */
-/* #include <ncurses.h> */ /* Screen handling and optimisation functions */
-/* #include <allegro.h> */ /* A game library for graphics, sounds, etc. */
-/* #include <libintl.h> */ /* Internationalization / translation */
-/* #include <locale.h> */ /* MACROS LC_ for location specific settings */
-/* #include "libeco.h" */ /* I/O, Math, Sound, Color, Portable Linux/Windows */
-/* #include "sudoku999.h" */ /* To be created for this template if needed */
 
 /* ---------------------------------------------------------------------- */
 /* definitions */
@@ -233,7 +132,3 @@ int tenta8(int w[9][9][9]);
 
 
 #endif /* NOT def _EXN_H */
-
-/* ---------------------------------------------------------------------- */
-/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
-/* Template by Dr. Beco <rcb at beco dot cc> Version 20160612.142044      */
